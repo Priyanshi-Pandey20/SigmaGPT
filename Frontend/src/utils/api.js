5601505992f606599497dd14fc152438431d3ae5
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-05-23 16:54:25 +0530

    backend deployed

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = "https://sigmagpt-backend-4ldy.onrender.com";
 
 export const apiFetch = (endpoint, options = {}) => {
   const token = localStorage.getItem("token");
